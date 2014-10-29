c2dce267b849cf14c40266277ff073af52c32c52
榊原吉伸 <sakura.colonnade@gmail.com>
2014-10-29 23:54:15 +0900

    チャージショットのクラス作成

---
+++ src/Shot/ChargeShot.cpp
@@ -0,0 +1,9 @@
+//
+//  ChargeShot.cpp
+//  simpleShootingGame
+//
+//  Created by 榊原吉伸 on 2014/10/29.
+//
+//
+
+#include "ChargeShot.h"
---
+++ src/Shot/ChargeShot.h
@@ -0,0 +1,28 @@
+//
+//  ChargeShot.h
+//  simpleShootingGame
+//
+//  Created by 榊原吉伸 on 2014/10/29.
+//
+//
+
+#ifndef __simpleShootingGame__ChargeShot__
+#define __simpleShootingGame__ChargeShot__
+
+#include <stdio.h>
+#include "AbstShot.h"
+
+class ChargeShot : public AbstShot{
+    
+private:
+    
+public:
+    
+    void    init();
+    void    update();
+    void    draw();
+    
+    
+};
+
+#endif /* defined(__simpleShootingGame__ChargeShot__) */
