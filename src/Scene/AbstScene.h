b2b261bb07d47fccd956ba82888ee03fb73e6fe3
榊原吉伸 <sakura.colonnade@gmail.com>
2014-11-03 01:11:45 +0900

    シーンの抽象クラスを作成。

---
+++ src/Scene/AbstScene.h
@@ -0,0 +1,24 @@
+//
+//  AbstScene.h
+//  simpleShootingGame
+//
+//  Created by 榊原吉伸 on 2014/11/03.
+//
+//
+
+#ifndef __simpleShootingGame__AbstScene__
+#define __simpleShootingGame__AbstScene__
+
+#include <stdio.h>
+#include <ofMain.h>
+
+class AbstScene{
+private:
+    
+public:
+    virtual void setup() = 0;
+    virtual void update() = 0;
+    virtual void draw() = 0;
+};
+
+#endif /* defined(__simpleShootingGame__AbstScene__) */
