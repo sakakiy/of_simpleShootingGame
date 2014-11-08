cc731b70d537a8f2996b9e271ebde7af3a4c112e
榊原吉伸 <sakura.colonnade@gmail.com>
2014-11-08 15:15:57 +0900

    新規ファイル追加
    - NewHeader.h 追加

---
+++ NewHeader.h
@@ -0,0 +1,13 @@
+//
+//  NewHeader.h
+//  simpleShootingGame
+//
+//  Created by 榊原吉伸 on 2014/11/08.
+//
+//
+
+#ifndef simpleShootingGame_NewHeader_h
+#define simpleShootingGame_NewHeader_h
+
+
+#endif
